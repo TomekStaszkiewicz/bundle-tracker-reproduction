ae5b82fa1d83741aaefeed739fe55d7b1ffa0435
Tomasz Staszkiewicz <tomasz.staszkiewicz@deployed.pl>
2021-06-01 11:43:27 +0200

    Working example on version 0.4.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,7 @@
 const path = require('path');
 const BundleTracker = require('webpack-bundle-tracker');
-const _ = require('underscore');
 
-const RelativeBundleTracker = function (options) {
-    BundleTracker.call(this, options);
-  };
-  RelativeBundleTracker.prototype = Object.create(BundleTracker.prototype);
-  RelativeBundleTracker.prototype.writeOutput = function (compiler, contents) {
-    const relativePathRoot = path.join(process.cwd(), 'dist', path.sep);
-  
-    _.forEach(contents.chunks, (bundle) => {
-      _.forEach(bundle, (chunk) => {
-        if (chunk.path.startsWith(relativePathRoot)) {
-          chunk.path = chunk.path.substr(relativePathRoot.length);
-        }
-      });
-    });
-    // contents.relativePathRoot = relativePathRoot;
-    BundleTracker.prototype.writeOutput.call(this, compiler, contents);
-  };
-
-const bundleTracker = new RelativeBundleTracker({
+const bundleTracker = new BundleTracker({
   filename: './bundle-stats.json',
 });
 
